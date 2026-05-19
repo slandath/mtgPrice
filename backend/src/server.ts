abcea80141eaeb319f619aa9cb32c97ac017b9df
slandath <jesus_slanda@yahoo.com>
2026-05-19 09:13:08 -0400

    more tweaks
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,8 @@ fastify.get('/', async () => {
  */
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    const port = Number(process.env.PORT || 3000);
+    await fastify.listen({ port, host: "0.0.0.0" })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
