781617dc278fdd43434d3a2682187de443d18ab6
slandath <jesus_slanda@yahoo.com>
2026-05-20 20:10:34 -0400

    table done
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -11,9 +11,31 @@ const { isPending, isFetching, isError, data, error } = useQuery({
 
 <template>
   <h1>Home</h1>
-  <span v-if="isPending || isFetching">Loading...</span>
+  <span v-if="isPending || isFetching" aria-busy="true">Loading</span>
   <span v-else-if="isError">Error: {{  error?.message }}</span>
-  <ul v-else-if="data">
-    <li v-for="item in data.inventory" :key="item.id">{{ item.name }}</li>
-  </ul>
+
+  <table v-else-if="data">
+    <thead>
+      <tr>
+        <th scope="col">Qty</th>
+        <th scope="col">Name</th>
+        <th scope="col">Current Price</th>
+        <th scope="col">Paid (Each)</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in data.inventory" :key="item.id">
+        <th scope="row">{{ item.quantity }}</th>
+        <td>{{ item.name }}</td>
+        <td>${{ item.currentPrice }}</td>
+        <td>${{ item.cost }}</td>
+      </tr>
+    </tbody>
+  </table>
+
+
+
+  <!-- <ul v-else-if="data"> -->
+    <!-- <li v-for="item in data.inventory" :key="item.id">{{ item.name }}</li>
+  </ul> -->
 </template>
