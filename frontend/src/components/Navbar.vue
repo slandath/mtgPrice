4825541c44e978fc331f3858862e246aac26f8c2
slandath <jesus_slanda@yahoo.com>
2026-05-29 21:43:42 -0400

    added tooltips on hover for navbar
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -23,8 +23,8 @@ async function handleSignOut() {
                 <li><h1><RouterLink to ="/">MTG Price Tracker</RouterLink></h1></li>
             </ul>
             <ul>
-                <li><button @click="router.push('/add')"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Lucide by Lucide Contributors - https://github.com/lucide-icons/lucide/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7-7v14"/></svg></button></li>
-                <li><button @click="handleSignOut"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Lucide by Lucide Contributors - https://github.com/lucide-icons/lucide/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16 17l5-5l-5-5m5 5H9m0 9H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/></svg></button></li>
+                <li><button @click="router.push('/add')" data-tooltip="Add Item" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Lucide by Lucide Contributors - https://github.com/lucide-icons/lucide/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7-7v14"/></svg></button></li>
+                <li><button @click="handleSignOut" data-tooltip="Logout" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Lucide by Lucide Contributors - https://github.com/lucide-icons/lucide/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16 17l5-5l-5-5m5 5H9m0 9H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/></svg></button></li>
             </ul>
         </nav>
     </header>
