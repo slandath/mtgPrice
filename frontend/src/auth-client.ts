3d0d83be2bc5924ba6947ff9c7583f4a54a703b6
slandath <jesus_slanda@yahoo.com>
2026-06-10 19:24:24 -0400

    changed baseURL
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- frontend/src/auth-client.ts
+++ frontend/src/auth-client.ts
@@ -2,7 +2,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/vue";
 
 export const authClient = createAuthClient({
-  baseURL: "/api/auth",
+  baseURL: `${window.location.origin}/api/auth`,
   fetchOptions: {},
   customFetch: (input: RequestInfo | URL, init = {}) =>
     fetch(input, { ...init, credentials: "include" }),
